e3c813520051d1a9ba58dccb5a88df7960a3ea32
Aaron Clark <aaronpclark92@gmail.com>
2020-05-07 15:06:46 -0500

    feaT: adDs flaG To COpy reSULtS tO cLIPbOaRd

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 #!/usr/bin/env node
 
-const argv = process.argv.slice(2)
+const argv = process.argv.slice(2);
+const clipboardy = require('clipboardy');
 
-const speeng = str => {
+const copy = (str) => {
+  clipboardy.write(str);
+};
+
+const speeng = (str) => {
   if (typeof str !== 'string') {
-    throw new Error('NoT A StRiNG!!')
+    throw new Error('NoT A StRiNG!!');
   }
 
   if (!str.length) {
-    throw new Error('ThAt Is NoT LoNg EnOuGH!!')
+    throw new Error('ThAt Is NoT LoNg EnOuGH!!');
   }
 
-  const length = str.length
-  const random = Array.from({ length }, () => Math.floor(Math.random() * 10))
+  const length = str.length;
+  const random = Array.from({ length }, () => Math.floor(Math.random() * 10));
 
-  const res = random.map((x, i) => caseMaker(str[i].toLowerCase(), x))
+  const res = random.map((x, i) => caseMaker(str[i].toLowerCase(), x));
 
-  return res.join('')
+  return res.join('');
+};
+
+const caseMaker = (c, v) => (v >= 5 ? c.toUpperCase() : c);
+
+
+// handles -c flag at beginning or end of string
+if ((argv[0] === '-c')) {
+  argv.shift();
+  const res = speeng(argv.join(' '));
+  copy(res);
+  console.log(res);
+} else if ((argv[argv.length - 1] = '-c')) {
+  argv.pop();
+  const res = speeng(argv.join(' '));
+  copy(res);
+  console.log(res);
+} else {
+  console.log(speeng(argv.join(' ')));
 }
 
-const caseMaker = (c, v) => (v >= 5 ? c.toUpperCase() : c)
-
-if (argv.length) console.log(speeng(argv.join(' ')))
-
-module.exports = speeng
+module.exports = speeng;
