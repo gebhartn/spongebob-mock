0fb436900cf384295349c463b99f95c362f10611
Aaron Clark <aaronpclark92@gmail.com>
2020-05-07 15:14:35 -0500

    FIx: CoRReCTS anTIQUatEd LiNTER

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 #!/usr/bin/env node
 
-const argv = process.argv.slice(2);
-const clipboardy = require('clipboardy');
+const argv = process.argv.slice(2)
+const clipboardy = require('clipboardy')
 
 const copy = (str) => {
-  clipboardy.write(str);
-};
+  clipboardy.write(str)
+}
 
 const speeng = (str) => {
   if (typeof str !== 'string') {
-    throw new Error('NoT A StRiNG!!');
+    throw new Error('NoT A StRiNG!!')
   }
 
   if (!str.length) {
-    throw new Error('ThAt Is NoT LoNg EnOuGH!!');
+    throw new Error('ThAt Is NoT LoNg EnOuGH!!')
   }
 
-  const length = str.length;
-  const random = Array.from({ length }, () => Math.floor(Math.random() * 10));
+  const length = str.length
+  const random = Array.from({ length }, () => Math.floor(Math.random() * 10))
 
-  const res = random.map((x, i) => caseMaker(str[i].toLowerCase(), x));
+  const res = random.map((x, i) => caseMaker(str[i].toLowerCase(), x))
 
-  return res.join('');
-};
+  return res.join('')
+}
 
-const caseMaker = (c, v) => (v >= 5 ? c.toUpperCase() : c);
+const caseMaker = (c, v) => (v >= 5 ? c.toUpperCase() : c)
 
 
 // handles -c flag at beginning or end of string
 if ((argv[0] === '-c')) {
-  argv.shift();
-  const res = speeng(argv.join(' '));
-  copy(res);
-  console.log(res);
+  argv.shift()
+  const res = speeng(argv.join(' '))
+  copy(res)
+  console.log(res)
 } else if ((argv[argv.length - 1] = '-c')) {
-  argv.pop();
-  const res = speeng(argv.join(' '));
-  copy(res);
+  argv.pop()
+  const res = speeng(argv.join(' '))
+  copy(res)
   console.log(res);
 } else {
-  console.log(speeng(argv.join(' ')));
+  console.log(speeng(argv.join(' ')))
 }
 
-module.exports = speeng;
+module.exports = speeng
